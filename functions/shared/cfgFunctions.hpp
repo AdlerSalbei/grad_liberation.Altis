7782c79b608f5ac59c04b584543ba5ff84f12057
Salbei <karsus3@web.de>
2018-04-05 22:40:33 +0200

    more

---
+++ functions/shared/cfgFunctions.hpp
@@ -0,0 +1,35 @@
+class grad_liberation {
+    class shared {
+        class aceActions {};
+        class adaptiveOpforFactor {};
+        class addAllVirtualCargo {};
+        class checkClass {};
+        class checkGear {};
+        class cleanOpforVehicle {};
+        class correctLaserBatteries {};
+        class crateCheckValue {};
+        class crateFromStorage {};
+        class createCrate {};
+        class diagnostic {};
+        class fetchParams {};
+        class fetchPermissions {};
+        class filterMods {};
+        class findOpforSpawnPoint {};
+        class forceBluforCrew {};
+        class getAdaptivSquadComp {};
+        class getAdaptivVehicle {};
+        class getCommander {};
+        class getCorrectedSectorRange {};
+        class getFobName {};
+        class getForceRatio {};
+        class getGroupType {};
+        class getLoadout {};
+        class getLocationName {};
+        class getMilitaryID {};
+        class getMobileRespawns {};
+        class getMulti {};
+        class getNearbyPlayers {};
+        class getNearestBluforObjective {};
+        class get
+    };
+};
